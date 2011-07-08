032748e704bfaa8532a0ed865de3929e1867253f
kuroda <kuroda@qnote.co.jp>
2011-07-08 20:29:50 +0900

    test

--- src/kenzou/jp/TestApp05Activity.java
+++ src/kenzou/jp/TestApp05Activity.java
@@ -4,7 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 
 /*
- * test
+ * testtesttest
  */
 public class TestApp05Activity extends Activity {
     /** Called when the activity is first created. */
