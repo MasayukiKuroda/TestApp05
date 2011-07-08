f619e1abd3844b5d5d132b2ea518b8430dd82818
kuroda <kuroda@qnote.co.jp>
2011-07-08 20:27:51 +0900

    initial commit

---
+++ src/kenzou/jp/TestApp05Activity.java
@@ -0,0 +1,16 @@
+package kenzou.jp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/*
+ * test
+ */
+public class TestApp05Activity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
